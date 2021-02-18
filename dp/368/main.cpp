12d6ac32e60fe5646e5254002bba2f71e93798b7
Stewie Yang <ysw1912>
2021-02-18 23:58:02 +0800

    add 368

--- dp/368/main.cpp
+++ dp/368/main.cpp
@@ -3,29 +3,51 @@
 
 #include <cassert>
 
+#include "utils.h"
+
 using namespace std;
 
-int countNumbersWithUniqueDigits(int n);
+vector<int> largestDivisibleSubset(vector<int>& nums);
 
 int main() {
-  assert(countNumbersWithUniqueDigits(2) == 91);
+  vector<int> nums = {1, 2, 3};
+  assert(largestDivisibleSubset(nums) == vector<int>({1, 2}));
+  nums = {1, 2, 4, 8};
+  assert(largestDivisibleSubset(nums) == vector<int>({1, 2, 4, 8}));
+  nums = {5, 9, 18, 54, 108, 540, 90, 180, 360, 720};
+  assert(largestDivisibleSubset(nums) == vector<int>({9, 18, 90, 180, 360, 720}));
 }
 
-int countNumbersWithUniqueDigits(int n) {
-  if (n == 0) {
-    return 1;
+vector<int> largestDivisibleSubset(vector<int>& nums) {
+  if (nums.empty()) {
+    return {};
   }
-  if (n == 1) {
-    return 10;
+  sort(nums.begin(), nums.end());
+  int size = nums.size();
+  vector<vector<int>> dp(2, vector<int>(size, 0));
+  for (int i = 0; i < size; i++) {
+    // Max length.
+    dp[0][i] = 1;
+    // Predecessor.
+    dp[1][i] = i;
   }
-  n = min(10, n);
-  vector<int> dp(n + 1);
-  dp[0] = 1;
-  dp[1] = 9;
-  int ans = 10;
-  for (int i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] * (10 - i + 1);
-    ans += dp[i];
+  for (int i = 1; i < size; i++) {
+    for (int j = 0; j < i; j++) {
+      if (nums[i] % nums[j] == 0 && dp[0][j] + 1 > dp[0][i]) {
+        dp[0][i] = dp[0][j] + 1;
+        dp[1][i] = j;
+      }
+    }
   }
-  return ans;
+  cout << dp << endl;
+  int idx = max_element(dp[0].begin(), dp[0].end()) - dp[0].begin();
+  int len = dp[0][idx];
+  vector<int> res;
+  for (int i = 0; i < len; i++) {
+    res.emplace_back(nums[idx]);
+    idx = dp[1][idx];
+  }
+  std::reverse(res.begin(), res.end());
+  cout << res << endl;
+  return res;
 }
